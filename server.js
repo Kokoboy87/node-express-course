a954c9bd4166b2385b2cb24bf2a2bc0c55f6a855
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2022-01-07 13:33:31 -0500

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
+
+const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }];
+
+app.get('/users', function () {
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		USERS: mockUserData,
+	});
+});
 
 app.listen(8000, function () {
 	console.log('server is running');
